56685edbb756996af23b74fee6464caaccc141c1
Zak <guetter.zakary@gmail.com>
2022-06-24 21:13:34 -0500

    Moved BarChart into a new file

--- src/Components/BarChart/BarChart.tsx
+++ src/Components/BarChart/BarChart.tsx
@@ -9,9 +9,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
-import { setLabels } from 'react-chartjs-2/dist/utils';
-import Slider from '@mui/material/Slider';
+import TopBar from '../TopBar/TopBar';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
@@ -53,7 +51,6 @@ function BarChart() {
     const [amount, setAmount] = useState(200);
     const [data, setData] = useState(getData(amount));
     const [labels, setLabels] = useState(getLabels(amount));
-    let currentAmount = amount;
     const finishedData = {
         labels: labels,
         responsive: true,
@@ -78,98 +75,7 @@ function BarChart() {
 
     return (
         <div className='barChart' style={{ width: '100%' }}>
-            <div
-                style={{
-                    display: 'flex',
-                    width: '100%',
-                    justifyContent: 'center',
-                }}>
-                <div
-                    style={{
-                        background: 'rgb(29,29,29)',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        paddingTop: 10,
-                        width: '40%',
-                        borderBottomLeftRadius: 10,
-                        borderBottomRightRadius: 10,
-                        justifyContent: 'center',
-                    }}>
-                    <div
-                        style={{
-                            textAlign: 'center',
-                            marginLeft: 25,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            paddingBottom: 15,
-                        }}>
-                        <span style={{ color: 'rgb(163,163,163)' }}>
-                            <b>Amt. of Columns</b>
-                        </span>
-                        <Slider
-                            aria-label='Always visible'
-                            defaultValue={amount}
-                            onChange={(e) => handleChange(e.target.value)}
-                            step={1}
-                            min={5}
-                            max={1000}
-                            style={{ width: '100%', paddingTop: 25 }}
-                        />
-                    </div>
-                    <div
-                        style={{
-                            textAlign: 'center',
-                            marginLeft: 25,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            paddingBottom: 20,
-                        }}>
-                        <span style={{ color: 'rgb(163,163,163)', paddingBottom: 5 }}>
-                            <b>Sort Algo</b>
-                        </span>
-                        <select style={{ borderRadius: 8, padding: 5 }}>
-                            <option>None</option>
-                            <option>Bubble</option>
-                            <option>Select</option>
-                            <option>Heap</option>
-                            <option>Merge</option>
-                            <option>Quick</option>
-                        </select>
-                    </div>
-                    <div
-                        style={{
-                            textAlign: 'center',
-                            marginLeft: 25,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            paddingBottom: 20,
-                        }}>
-                        <span style={{ color: 'rgb(163,163,163)', paddingBottom: 5 }}>
-                            <b>Search Algo</b>
-                        </span>
-                        <select style={{ borderRadius: 8, padding: 5 }}>
-                            <option>None</option>
-                            <option>Linear</option>
-                            <option>Binary</option>
-                        </select>
-                    </div>
-                    <div style={{ alignSelf: 'center', marginLeft: 20, float: 'right' }}>
-                        <button
-                            style={{
-                                padding: 5,
-                                paddingRight: 14,
-                                paddingLeft: 14,
-                                fontSize: 18,
-                                backgroundColor: 'rgb(25,118,210)',
-                                color: '#FFFFFF',
-                                border: 'none',
-                                borderRadius: 8,
-                            }}>
-                            <b>Start</b>
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <TopBar handleChange={handleChange} amount={amount} />
             <Bar options={options} data={finishedData} style={{ position: 'fixed', bottom: 5 }} />
         </div>
     );
---
+++ src/Components/TopBar/TopBar.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import Slider from '@mui/material/Slider';
+
+function TopBar({ handleChange, amount }) {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'center',
+            }}>
+            <div
+                style={{
+                    background: 'rgb(29,29,29)',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    paddingTop: 10,
+                    width: '40%',
+                    borderBottomLeftRadius: 10,
+                    borderBottomRightRadius: 10,
+                    justifyContent: 'center',
+                }}>
+                <div
+                    style={{
+                        textAlign: 'center',
+                        marginLeft: 25,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingBottom: 15,
+                    }}>
+                    <span style={{ color: 'rgb(163,163,163)' }}>
+                        <b>Amt. of Columns</b>
+                    </span>
+                    <Slider
+                        aria-label='Always visible'
+                        defaultValue={amount}
+                        onChange={(e) => handleChange(e.target.value)}
+                        step={1}
+                        min={5}
+                        max={1000}
+                        style={{ width: '100%', paddingTop: 25 }}
+                    />
+                </div>
+                <div
+                    style={{
+                        textAlign: 'center',
+                        marginLeft: 25,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingBottom: 20,
+                    }}>
+                    <span style={{ color: 'rgb(163,163,163)', paddingBottom: 5 }}>
+                        <b>Sort Algo</b>
+                    </span>
+                    <select style={{ borderRadius: 8, padding: 5 }}>
+                        <option>None</option>
+                        <option>Bubble</option>
+                        <option>Select</option>
+                        <option>Heap</option>
+                        <option>Merge</option>
+                        <option>Quick</option>
+                    </select>
+                </div>
+                <div
+                    style={{
+                        textAlign: 'center',
+                        marginLeft: 25,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingBottom: 20,
+                    }}>
+                    <span style={{ color: 'rgb(163,163,163)', paddingBottom: 5 }}>
+                        <b>Search Algo</b>
+                    </span>
+                    <select style={{ borderRadius: 8, padding: 5 }}>
+                        <option>None</option>
+                        <option>Linear</option>
+                        <option>Binary</option>
+                    </select>
+                </div>
+                <div
+                    style={{
+                        textAlign: 'center',
+                        marginLeft: 25,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        paddingBottom: 20,
+                    }}>
+                    <span style={{ color: 'rgb(163,163,163)', paddingBottom: 5 }}>
+                        <b>Number to Find</b>
+                    </span>
+                    <select style={{ borderRadius: 8, padding: 5 }}></select>
+                </div>
+                <div style={{ alignSelf: 'center', marginLeft: 20, float: 'right' }}>
+                    <button
+                        style={{
+                            padding: 5,
+                            paddingRight: 14,
+                            paddingLeft: 14,
+                            fontSize: 18,
+                            backgroundColor: 'rgb(25,118,210)',
+                            color: '#FFFFFF',
+                            border: 'none',
+                            borderRadius: 8,
+                        }}>
+                        <b>Start</b>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TopBar;
